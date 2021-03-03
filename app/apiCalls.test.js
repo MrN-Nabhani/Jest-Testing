c4ba8db15259a9a1cdeb492b1410e606d0fee9b2
MrN-Nabhani <nabhaninajm@gmail.com>
2021-03-03 23:00:37 +0200

    updated test to match needs

--- app/apiCalls.test.js
+++ app/apiCalls.test.js
@@ -3,9 +3,8 @@ test("should getTodos exist", () => {
   expect(getTodos).toBeDefined();
 });
 
-test('should get todos array, first having a title of "delectus aut autem" ', async () => {
-  // expect.assertions(1);
+test('should get todos array with 200 element" ', async () => {
+  expect.assertions(1);
   const data = await getTodos();
-  // expect(data[0].title).toEqual("delectus aut autem");
   expect(data.length).toEqual(200);
 });
